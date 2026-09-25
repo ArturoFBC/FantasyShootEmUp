8022a129274cae63567bdf7a4f897afb5185638f
ArturoFBC <arturo.fbc@gmail.com>
2026-09-25 10:02:33 +0200

    Bugs with player setup fixed

--- scenes/player/player_setup.gd
+++ scenes/player/player_setup.gd
@@ -9,8 +9,14 @@ func _ready() -> void:
 	var player_class = GlobalData.current_character_class
 	var player_model: Node3D = player_class.graphic.instantiate()
 	base_player_node.add_child.call_deferred(player_model)
-	player_model.rotate(Vector3(0,1,0), 180)
 	player_model.scale = (Vector3(1.5,1.5,1.5))
 	flicker_effect.my_mesh = player_model
-	base_player_node.add_child.call_deferred(player_class.main_weapons[0].instantiate())
-	base_player_node.add_child.call_deferred(player_class.off_weapons[0].instantiate())
+	
+	var main_weapon = player_class.main_weapons[0].instantiate()
+	main_weapon.rotate(Vector3(0,1,0), PI)
+	base_player_node.add_child.call_deferred(main_weapon)
+	var off_weapon: Node3D = player_class.off_weapons[0].instantiate()
+	print(off_weapon.rotation_degrees)
+	off_weapon.rotate(Vector3(0,1,0), PI)
+	base_player_node.add_child.call_deferred(off_weapon)
+	print(off_weapon.rotation_degrees)
